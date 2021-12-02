193b53ea91adb129183a94caf60257bbc3039fc3
gietema <jochem.gietema@onfido.com>
2021-12-02 20:44:36 +0000

    Add day two

---
+++ day_two.py
@@ -0,0 +1,40 @@
+from typing import List
+
+
+def get_moves() -> List[List[str]]:
+    with open("day_two.txt", "r") as f:
+        return [line.rstrip().split(" ") for line in f.readlines()]
+
+def part_one() -> int:
+    hor_pos, depth = 0, 0
+    for move in get_moves():
+        move_type, move = move
+        move = int(move)
+        match move_type:
+            case "forward":
+                hor_pos += move
+            case "down":
+                depth += move
+            case "up":
+                depth -= move
+    return hor_pos * depth
+
+
+def part_two() -> int:
+    hor_pos, depth, aim = 0, 0, 0
+    for move in get_moves():
+        move_type, move = move
+        move = int(move)
+        match move_type:
+            case "forward":
+                hor_pos += move
+                depth += (aim * move)
+            case "down":
+                aim += move
+            case "up":
+                aim -= move
+    return hor_pos * depth
+
+
+if __name__ == "__main__":
+    print(f"Solution part one: {part_one()} \nSolution part two: {part_two()}")
