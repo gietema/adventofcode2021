a63b73aeb148f1dd82da4ebd6240721c999621b4
Thijs Baaijen <Thijss@users.noreply.github.com>
2021-12-07 23:40:00 +0100

    refactor

--- days/day_07.py
+++ days/day_07.py
@@ -1,40 +1,37 @@
 from time import time
+from typing import Callable
 
 
 def move_linear(current: int, to: int):
     return abs(current - to)
 
 
-def move_increment(current: int, to: int):
+def move_triangular(current: int, to: int):
     moves = abs(current - to)
     return sum(range(moves + 1))
 
 
-def part_1(crabs: list[int]):
+def calculate_fuel_costs(crabs: list[int], cost_func: Callable):
+
     left = min(crabs)
     right = max(crabs) + 1
 
-    least_fuel = right * len(crabs)
+    least_fuel = 9e20
+
     for position in range(left, right):
-        fuel_cost = sum([move_linear(current=crab, to=position) for crab in crabs])
+        fuel_cost = sum([cost_func(current=crab, to=position) for crab in crabs])
         if fuel_cost < least_fuel:
             least_fuel = fuel_cost
 
     return least_fuel
+
+
+def part_1(crabs: list[int]):
+    return calculate_fuel_costs(crabs, move_linear)
 
 
 def part_2(crabs: list[int]):
-    left = min(crabs)
-    right = max(crabs) + 1
-
-    least_fuel = 9e20  # big number
-
-    for position in range(left, right):
-        fuel_cost = sum([move_increment(current=crab, to=position) for crab in crabs])
-        if fuel_cost < least_fuel:
-            least_fuel = fuel_cost
-
-    return least_fuel
+    return calculate_fuel_costs(crabs, move_triangular)
 
 
 if __name__ == "__main__":
