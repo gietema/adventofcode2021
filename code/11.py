2abc8cd11db953607bbb59206f9150cc5084dafb
gietema <jochem.gietema@onfido.com>
2021-12-11 10:20:24 +0000

    Refactor, update np

--- code/11.py
+++ code/11.py
@@ -3,11 +3,11 @@ from itertools import chain, combinations
 from pathlib import Path
 
 
-def get_adjacent(idx_line, idx):
+def get_adjacent(y, x):
     return [
         (new_y, new_x)
-        for x, y in set(combinations([-1, -1, 0, 1, 1, 0, -1], 2))
-        if 0 <= (new_y := (idx_line + y)) < 10 and 0 <= (new_x := (idx + x)) < 10 and not (x == 0 and y == 0)
+        for i, j in set(combinations([-1, -1, 0, 1, 1, 0, -1], 2))
+        if 0 <= (new_y := (y + j)) < 10 and 0 <= (new_x := (x + i)) < 10 and not (i == 0 and j == 0)
     ]
 
 
@@ -18,26 +18,34 @@ def increase_energy_level(octopuses: list[list[int]]) -> list[list[int]]:
     return octopuses
 
 
+def should_flash(octopuses):
+    return any([o > 9 for o in chain(*octopuses)])
+
+
+def all_flashed(octopuses):
+    return all([octopus == 0 for octopus in chain(*octopuses)])
+
+
 def solve(octopuses: list[list[int]], part_one: bool = False) -> int:
-    octopuses = copy.deepcopy(octopuses)
-    num_flashes, steps = 0, 0
-    while not all([octopus == 0 for octopus in chain(*octopuses)]):
-        steps += 1
-        flashed_octopuses = []
-        octopuses = increase_energy_level(octopuses)
-        while any([o > 9 for o in chain(*octopuses)]):
-            for idx_line, line in enumerate(octopuses):
-                for idx, o in enumerate(line):
-                    if o > 9 and (idx_line, idx) not in flashed_octopuses:
-                        for adjacent in get_adjacent(idx_line, idx):
-                            if adjacent not in flashed_octopuses:
-                                octopuses[adjacent[0]][adjacent[1]] += 1
-                        octopuses[idx_line][idx] = 0
-                        flashed_octopuses.append((idx_line, idx))
+    octopuses, num_flashes, steps = copy.deepcopy(octopuses), 0, 0
+    while not all_flashed(octopuses):
+        steps, octopuses, flashed_octopuses = steps + 1, increase_energy_level(octopuses), []
+        while should_flash(octopuses):
+            octopuses, flashed_octopuses = flash(octopuses, flashed_octopuses)
         num_flashes += len(flashed_octopuses)
         if part_one and steps == 100:
             return num_flashes
     return steps
+
+
+def flash(octopuses, flashed_octopuses):
+    for idx_line, line in enumerate(octopuses):
+        for idx, o in filter(lambda x: x[1] > 9 and (idx_line, x[0]) not in flashed_octopuses, enumerate(line)):
+            for adjacent in filter(lambda x: x not in flashed_octopuses, get_adjacent(idx_line, idx)):
+                octopuses[adjacent[0]][adjacent[1]] += 1
+            octopuses[idx_line][idx] = 0
+            flashed_octopuses.append((idx_line, idx))
+    return octopuses, flashed_octopuses
 
 
 if __name__ == "__main__":
